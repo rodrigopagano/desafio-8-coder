bbd7bc3b4db4787a4f9425bff717706fcd43fa7f
rodrigopagano <94488888+rodrigopagano@users.noreply.github.com>
2023-03-06 09:27:58 -0300

    Delete src directory

--- src/routes/session.routes.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require('express')
-const sessionController = require('../controller/session.controller')
-const router = Router()
-
-router.post('/register',sessionController.registerLogin);
-
-module.exports = router;
